ba45b993937c8547088b86d46c996d8b28d0d828
Klas <klas.hasselquist@gmail.com>
2016-03-31 00:04:54 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('app', []);
-app.controller('todosCtrl', function($scope) {
-    $scope.todos = {};
-	$scope.addTodo = function(e) {
-		$scope.todos.push(e.target.value);
-	}
-});
